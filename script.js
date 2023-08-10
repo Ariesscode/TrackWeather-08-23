972942abe8662390bc08e836846c2b426783f3d7
Abigail Burtley <abigailtop95@yahoo.com>
2023-08-10 17:55:40 -0400

    add a function for search click event to fetch data from openweater api to gather data to display weather conditions and parse the data into json format

--- script.js
+++ script.js
@@ -14,5 +14,19 @@ searchButton.addEventListener('click', () => {
     var cityDisplay = searchInput.value;
     if (cityDisplay) {
         fetchWeatherData(cityDisplay);
+        
     }
 });
+
+async function fetchWeatherData(city) {
+    try{
+        const key = "6ed102388c0f7d2090336e3ef5fc46dd";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`; 
+        const weatherData = await fetch(`${url}`).then(response => response.json()); //Async method to fetch data from openweather Api,
+        console.log(weatherData);                                                    //this allows the promise to be resolved first before executing my funtion,
+    } catch(err) {                                                                   //so that data is retrieved and I can catch errors in the retrieving process
+        console.log(err);                                                             //response.json is used to parse the data into a readable javascript object 
+
+    }
+}
+
