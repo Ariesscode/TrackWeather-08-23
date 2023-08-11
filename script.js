28bed3e743433ada4125daed3f1d5438cd649258
Abigail Burtley <abigailtop95@yahoo.com>
2023-08-10 23:58:17 -0400

    I will retrieve the data from 5 day forcast to display into html

--- script.js
+++ script.js
@@ -18,22 +18,27 @@ searchButton.addEventListener('click', () => {
     }
 });
 
-async function fetchWeatherData(city) {
+
+//Async method to fetch data from openweather Api,
+    //this allows the promise to be resolved first before executing my funtion,
+    //so that data is retrieved and I can catch errors in the retrieving process
+    //response.json is used to make readable object of javascript
+async function fetchWeatherData(city) { 
+    
     try{
         const key = "6ed102388c0f7d2090336e3ef5fc46dd";
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`; 
         const weatherData = await fetch(`${url}`).then(response => response.json()); 
         console.log(weatherData);   
-        let latitude = weatherData.coord.lat;
-        let longitude = weatherData.coord.lon;
-        console.log(latitude,longitude); // here we have the longitude and latitude values needed for the geoloacation api
+        let lat = weatherData.coord.lat;
+        let lon = weatherData.coord.lon;
+        console.log(lat,lon); // here we have the longitude and latitude values needed for the geoloacation api
+      
         
-        var units = 'metric';
-        var lang = 'en';
-        
-        
-        var forcastUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=${units}&lang=${lang}`;
+        const forcastKey = "6ed102388c0f7d2090336e3ef5fc46dd";
+        const forcastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
         //two objects should be listed in console with weather data that will be extracted from
+        //forcastWeatherData hold all the five day forcast
         var forcastWeatherData = await fetch(`${forcastUrl}`).then(response => response.json()); 
         
         console.log(forcastWeatherData);
@@ -44,7 +49,3 @@ async function fetchWeatherData(city) {
     }
 }
 
-//Async method to fetch data from openweather Api,
-//this allows the promise to be resolved first before executing my funtion,
-//so that data is retrieved and I can catch errors in the retrieving process
-//response.json is used to make readable object of javascript
