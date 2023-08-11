ba906cce7c7e91925628a242cc8eaba6960d1eec
Abigail Burtley <abigailtop95@yahoo.com>
2023-08-10 22:55:17 -0400

    console logged the latitude and longitude coord to validate their values, these will be used in geo locator api to retrieve a five day forcast of user city

--- script.js
+++ script.js
@@ -22,11 +22,26 @@ async function fetchWeatherData(city) {
     try{
         const key = "6ed102388c0f7d2090336e3ef5fc46dd";
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`; 
-        const weatherData = await fetch(`${url}`).then(response => response.json()); //Async method to fetch data from openweather Api,
-        console.log(weatherData);                                                    //this allows the promise to be resolved first before executing my funtion,
-    } catch(err) {                                                                   //so that data is retrieved and I can catch errors in the retrieving process
-        console.log(err);                                                             //response.json is used to parse the data into a readable javascript object 
+        const weatherData = await fetch(`${url}`).then(response => response.json()); 
+        console.log(weatherData);   
+        let latitude = weatherData.coord.lat;
+        let longitude = weatherData.coord.lon;
+        console.log(latitude,longitude); // here we have the longitude and latitude values needed for the geoloacation api
+        
+        var units = 'metric';
+        var lang = 'en';
+        
+        
+        var forcastUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=${units}&lang=${lang}`;
+        
+        
+    } catch(err) {                                                                   
+        console.log(err);                                                              
 
     }
 }
 
+//Async method to fetch data from openweather Api,
+//this allows the promise to be resolved first before executing my funtion,
+//so that data is retrieved and I can catch errors in the retrieving process
+//response.json is used to make readable object of javascript
