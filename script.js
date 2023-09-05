a764743dfc881f65d57a56ba1fe15bb3410199dc
Abigail Burtley <abigailtop95@yahoo.com>
2023-09-05 03:49:09 -0400

    set element to display the time

--- script.js
+++ script.js
@@ -71,7 +71,8 @@ function currentTime() {
     mm = (mm < 10) ? "0" + mm : mm;
     ss = (ss < 10) ? "0" + ss : ss;
 
-    
+    let time = hh + ":" + mm + ":" + ss + " " + session;
+    document.getElementById("clock").innerText = time;
 }
 
 
